2bbd4ee54fa27b895927a1ac460293cc9266b04f
samuel <samuelkelvinmwangi@gmail.com>
2023-07-29 23:37:38 +0300

    adding routes for runner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,26 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #  Routes for the Runner model
+  get '/runners' do
+    content_type :json
+    runners = Runner.all
+    runners.to_json
+  end
+
+  # Get a specific runner
+  get '/runners/:id' do
+  content_type :json
+  runner = Runner.find(params[:id])
+  runner.to_json
+  end
+  
+  # create a new runner
+  post '/runners' do
+    content_type :json
+    data = JSON.parse(request.body.read)
+    runner = Runner.create(data)
+    runner.to_json
+  end
+
 end

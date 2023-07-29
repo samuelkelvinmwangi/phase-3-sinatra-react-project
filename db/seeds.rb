7b30258f56a3f48c0e47fe06f8eab35bca09f20b
samuel <samuelkelvinmwangi@gmail.com>
2023-07-29 21:57:44 +0300

    adding fake code and seeding runner data in seeds filr

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
-puts "🌱 Seeding spices..."
+require 'faker'
 
-# Seed your database here
+puts "🌱 Seeding spices..."
+# clear existing data
+Runner.destroy.all
+Race.destroy.all
+
+# creating runners
+5.times do
+    Runner.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone_number: Faker::PhoneNumber.phone_number
+    )
 
 puts "✅ Done seeding!"
